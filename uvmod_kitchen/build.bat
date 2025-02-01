879b48de9ff58142bcadd25b03a3b7552c33944c
BlueDevil2 <mckinney.brandon@gmail.com>
2025-02-01 12:29:53 -0800

    Update build.bat

--- uvmod_kitchen/build.bat
+++ uvmod_kitchen/build.bat
@@ -10,6 +10,7 @@ python qsfirm.py unpack k5_v2.01.26_publish.bin temp\fw.dec.bin temp\fw.ver.bin
 :: mods by https://github.com/piotr022
 :: please choose only one of them and always 
 :: place as first mod in this batch file
+rem python mod_enable_tx_50to850.py temp\fw.dec.bin
 rem mod_piotr022_rssi_sbar.py temp\fw.dec.bin
 rem mod_piotr022_rssi_printer.py temp\fw.dec.bin
 
@@ -33,7 +34,6 @@ rem python mod_custom_steps.py temp\fw.dec.bin
     python mod_more_freq_steps_and_backlight_duration.py temp\fw.dec.bin
 
 rem python mod_disable_tx_completely.py temp\fw.dec.bin
-rem python mod_enable_tx_50to850.py temp\fw.dec.bin
 rem python mod_enable_tx_50to850_except_airband.py temp\fw.dec.bin
 
 rem python mod_menu_strings.py temp\fw.dec.bin
